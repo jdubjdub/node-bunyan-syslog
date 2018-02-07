b591c6a34483a7f2ed05ec39811e83451eea6976
JW Tsu <jtsu@electricimp.com>
2018-02-07 12:00:24 -0800

    Add index.d.ts file

---
+++ index.d.ts
@@ -0,0 +1,6 @@
+// Type definitions for bunyan-syslog 0.3.1
+// Project: https://github.com/joyent/node-bunyan-syslog
+// Definitions by: JW Tsu <https://github.com/jdubjdub>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare module 'bunyan-syslog';
